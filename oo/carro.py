392683a0d67465837566e6be8b4f56ec5478f281
Zeca Figueiredo <zecafig@me.com>
2019-08-18 15:58:57 -0300

    Update carro.py

--- oo/carro.py
+++ oo/carro.py
@@ -80,18 +80,40 @@ S   L
     >>> carro.calcular_velocidade()
     0
     >>> carro.calcular_direcao()
-    >>> 'Norte'
+    'Norte'
     >>> direcao.girar_a_direita()
     >>> carro.calcular_direcao()
-    >>> 'Leste'
+    'Leste'
     >>> direcao.girar_a_esquerda()
     >>> carro.calcular_direcao()
-    >>> 'Norte'
+    'Norte'
     >>> direcao.girar_a_esquerda()
     >>> carro.calcular_direcao()
-    >>> 'Oeste'
+    'Oeste'
 """
+class Carro: 
+    def __init__(self, direcao, motor):
+        self.direcao = direcao
+        self.motor = motor
 
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+        
+    def frear(self):
+        self.motor.frear()
+        
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
+      
 class Motor:
     def __init__(self):
         self.velocidade = 0
@@ -116,7 +138,6 @@ class Direcao:
     rotacao_a_esquerda_dct = {
         NORTE: OESTE, OESTE: SUL, SUL: LESTE, LESTE: NORTE
     }
-
 
     def __init__(self):
         self.valor = NORTE

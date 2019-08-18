71fd622814e1e714a179a674ce692dee1094b8ee
Zeca Figueiredo <zecafig@me.com>
2019-08-18 11:15:33 -0300

    Decorator @staticmethod e @classmethod

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,6 +9,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+    
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'   
+
 if __name__ == '__main__':
     renzo = Pessoa(nome='Renzo')
     luciano = Pessoa(renzo, nome='Luciano')
@@ -30,4 +38,4 @@ if __name__ == '__main__':
     print(luciano.olhos)
     print(renzo.olhos)
     print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))
-    
+    print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe)

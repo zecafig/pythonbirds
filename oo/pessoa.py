96cfc064d6a4df53fd3d5a296d067c7eec824922
zecafig <mindingo@macdingo.local>
2021-01-27 15:58:20 -0300

    Update pessoa.py

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,33 +1,34 @@
 class Pessoa:
-    olhos = 2 
+    olhos = 2
 
-    def __init__(self, *filhos, nome = None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f"Olá {id(self)}"
 
     @staticmethod
     def metodo_estatico():
         return 42
-    
+
     @classmethod
     def nome_e_atributos_de_classe(cls):
-        return f'{cls} - olhos {cls.olhos}'   
+        return f"{cls} - olhos {cls.olhos}"
 
-if __name__ == '__main__':
-    renzo = Pessoa(nome='Renzo')
-    luciano = Pessoa(renzo, nome='Luciano')
+
+if __name__ == "__main__":
+    renzo = Pessoa(nome="Renzo")
+    luciano = Pessoa(renzo, nome="Luciano")
     print(Pessoa.cumprimentar(luciano))
     print(id(luciano))
     print(luciano.cumprimentar())
-    print(luciano.nome) 
+    print(luciano.nome)
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
-    luciano.sobrenome = 'Ramalho'
+    luciano.sobrenome = "Ramalho"
     del luciano.filhos
     luciano.olhos = 1
     del luciano.olhos
